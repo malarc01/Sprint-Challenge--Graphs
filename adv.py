8bd1e2075fba021d350a6c8606d14e914d5330b6
malarc01 <email@example.com>
2020-05-04 19:10:41 -0400

    comment out the prints

--- adv.py
+++ adv.py
@@ -67,23 +67,23 @@ traversal_path = []
 def allrooms(player):
 
     def breadth_for_search(graph, v1, v2):
-        print((" "*2), "breadth_for_search() excueted")
+        # print((" "*2), "breadth_for_search() excueted")
 
         plan_to_visit = Queue()
-        print((" "*2), "plan_to_visit=>", plan_to_visit)
+        # print((" "*2), "plan_to_visit=>", plan_to_visit)
 
         plan_to_visit.enqueue([(v1, None)])
-        print((" "*2), "plan_to_visit.queue=>", plan_to_visit.queue)
+        # print((" "*2), "plan_to_visit.queue=>", plan_to_visit.queue)
         visited = set()
 
         while plan_to_visit.size() > 0:
 
             path = plan_to_visit.dequeue()
 
-            print((" "*3), "path=>", path)
+            # print((" "*3), "path=>", path)
             # GRAB THE VERTEX FROM THE END OF THE PATH
             vertex = path[-1][0]
-            print((" "*3), "vertex=>", vertex)
+            # print((" "*3), "vertex=>", vertex)
 
             if vertex not in visited:
 
@@ -91,13 +91,13 @@ def allrooms(player):
 
                 if vertex == v2:
                     # IF SO, RETURN THE PATH
-                    print((" "*5), " return path =>", path)
+                    # print((" "*5), " return path =>", path)
                     return path
                 # Enqueue A PATH TO all it's neighbors
                 for direction, room in graph[vertex].items():
                     # MAKE A COPY OF THE PATH
-                    print((" "*5), "direction=>", direction)
-                    print((" "*5), "room=>", room)
+                    # print((" "*5), "direction=>", direction)
+                    # print((" "*5), "room=>", room)
                     path_copy = path.copy()
 
                     # append neighbor
@@ -106,7 +106,6 @@ def allrooms(player):
                     plan_to_visit.enqueue(path_copy)
     print()
 
-    # make set of visited rooms and graph
     visited = set()
     graph = dict()
 
@@ -116,24 +115,25 @@ def allrooms(player):
     # define opposite rooms
     mirror = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
     while True:
-        # until you hit a dead end
+        # backtracking
         while previous_room != player.current_room.id:
-            print((" "*3), 'player.current_room.id=>', player.current_room.id)
+            # print((" "*3), 'player.current_room.id=>', player.current_room.id)
             visited.add(player.current_room.id)
-            # get list of current exits
+            # print((" "*3), "visited=>", visited)
+            # get list of exits for room
             exits = player.current_room.get_exits()
-
-            print((" "*3), "exits=>", exits)
+            # print((" "*3), "graph=>", graph)
+            # print((" "*3), "exits=>", exits)
 
             # if not visited before
             if player.current_room.id not in graph:
                 # add to graph
                 graph[player.current_room.id] = dict()
-                print((" "*4), "graph[player.current_room.id]=>",
-                      graph[player.current_room.id])
+                # print((" "*4), "graph[player.current_room.id]=>",
+                #       graph[player.current_room.id])
                 # add exits to graph
                 for exit in exits:
-                    print((" "*5), "exit=>", exit)
+                    # print((" "*5), "exit=>", exit)
                     graph[player.current_room.id][exit] = '?'
 
             # var with/ the previous room into current room's entry
@@ -144,25 +144,27 @@ def allrooms(player):
             if previous_room is not None:
                 graph[previous_room.id][direction_arrived_from] = player.current_room.id
 
-            # find ?
+            # find ? room
             for direction, room in graph[player.current_room.id].items():
+                # print((" "*4), "direction=>", direction)
+                # print((" "*4), "room=>", room)
                 possible_exits = []
                 if room == '?':
                     possible_exits.append(direction)
             if len(possible_exits) > 0:
                 direction_we_are_going = possible_exits.pop()
-                # update pointers
+
                 previous_room = player.current_room
                 direction_arrived_from = direction_we_are_going
-                # add direction travelled to traversal list, and travel
+                # output traversal list
                 traversal_path.append(direction_we_are_going)
-                print("traversal_path=>", traversal_path)
+                # print("traversal_path=>", traversal_path)
 
                 player.travel(direction_we_are_going)
             else:
                 break
 
-        # do BFS to find route to nearest unexplored room
+        # breadth_for_search
         travel_directions = breadth_for_search(
             graph, player.current_room.id, '?')
 
@@ -171,18 +173,18 @@ def allrooms(player):
             break
 
         # remove first room which is redundant bc it is current room
-        travel_directions.pop(0)
+        # travel_directions.pop(0)
         # follow travel_directions
         for room, direction in travel_directions:
-            print((" "*3), "END:room=>", room)
-            print((" "*3), "END:direction=>", direction)
-            print((" "*3), 'END:player.current_room.id=>', player.current_room.id)
+            # print((" "*3), "END:room=>", room)
+            # print((" "*3), "END:direction=>", direction)
+            # print((" "*3), 'END:player.current_room.id=>', player.current_room.id)
 
-            # update pointers
+            # update rooms
             previous_room = player.current_room
             direction_arrived_from = direction
 
-            # traverse the path and log it
+            # traversal_path
             player.travel(direction)
             traversal_path.append(direction)
 
